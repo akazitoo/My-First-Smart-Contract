a846ef892a39e581b13075afa31331272bf2addd
akazitoo <akdsdev@gmail.com>
2024-03-12 17:01:45 -0300

    my first smart contract finished

---
+++ test/Inbox.test.js
@@ -0,0 +1,58 @@
+const { Web3 } = require('web3');
+const ganache = require('ganache');
+const assert = require('assert');
+const web3 = new Web3(ganache.provider());
+const {interface, bytecode} = require('../compile');
+
+let accounts;
+let inbox;
+
+beforeEach(async() => {
+        //Get a list of all accounts
+        accounts = await web3.eth.getAccounts();
+
+        inbox = await new web3.eth.Contract(JSON.parse(interface))
+                .deploy({data: bytecode, arguments: ['hi there']})
+                .send({from: accounts[0], gas: '1000000'})
+});
+
+describe('Inbox', () => {
+        it('deploys a contract', ()=> {
+                assert.ok(inbox.options.address)
+        });
+        it('has a default message', async ()=> {
+                const message = await inbox.methods.message().call();
+                assert.equal(message, 'hi there');
+        })
+        it('can change the message', async ()=> {
+                await inbox.methods.setMessage('goodbye').send({from: accounts[0]});
+                const message = await inbox.methods.message().call();
+                assert.equal(message, 'goodbye');
+        })
+});
+
+// class Car {
+//         park(){
+//                 return 'stopped';
+//         }
+
+//         drive(){
+//                 return 'go';
+//         }
+// }
+
+// let car;
+
+// beforeEach(() =>{
+//         car = new Car();
+// });
+
+// describe('Car', () => {
+//         it('can park', () => {
+//                 assert.equal(car.park(), 'stopped');
+//         });
+
+//         it('can drive', () => {
+//                 assert.equal(car.drive(), 'go');
+//         });
+// });
